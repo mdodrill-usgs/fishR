ae37e69ec362ae64d178c96580ffb07ec4765712
mdodrill-usgs <mdodrill@usgs.gov>
2018-05-03 11:42:08 -0700

    coded this to look at M drive and copy db to local dir

--- R/connect_fish_db.R
+++ R/connect_fish_db.R
@@ -5,32 +5,37 @@
 #' @export
 #' @author Michael J. Dodrill, \email{mdodrill@usgs.gov}
 
-# need to update, make this function run !!!!!!!!
 
 connect_fish_db = function(update.db = FALSE){
+  # Note: should redo this code, see: https://stackoverflow.com/questions/43620927/dplyr-auto-disconnecting-postgres-connection-issue
+
   # library(RSQLite)
   # library(dplyr)
 
   if(update.db == TRUE){
-    update.db()
+    update_db()
   }
 
-  # path to sqlite db (there should only be one db in the folder)
-  db.name = list.files(path = data.dir, pattern = ".sqlite3")
-  my_path = paste(data.dir, db.name, sep = "/")
+  db.dir.loc <- paste0(find.package('fishR'), '/DataBase')
+  db.dir.sq.m = "M:/DATABASE/BIOLOGICAL/FISH/DATABASE_R_SQLite"
 
-  # check to see if there's a db in the data folder, if not, then create one
-  if(file.exists(my_path) == FALSE){
-    message("MD: No sqlite database exists, so making a new one.")
-    update.db()
+  # if the dir doesn't exist, make one and copy the db into it
+  if(dir.exists(path = db.dir.loc) == FALSE){
+    message("MD: No sqlite database exists locally, so copying from M:")
+    dir.create(db.dir.loc, showWarnings = FALSE)
 
-    # need these to be updated
-    db.name = list.files(path = data.dir, pattern = ".sqlite3")
-    my_path = paste(data.dir, db.name, sep = "/")
+    sq.name = list.files(path = db.dir.sq.m, pattern = ".sqlite3")
+    sq.path.from = paste0(db.dir.sq.m, '/', sq.name)
+    sq.path.to = paste0(db.dir.loc, '/', sq.name)
+
+    file.copy(sq.path.from, sq.path.to)
   }
+
+  sq.name = list.files(path = db.dir.loc, pattern = ".sqlite3")
+  sq.path = paste0(db.dir.loc, '/', sq.name)
 
   # connect to db
-  my_db = src_sqlite(path = my_path, create = FALSE)
+  my_db = dplyr::src_sqlite(path = sq.path, create = FALSE)
 
  return(my_db)
 }
